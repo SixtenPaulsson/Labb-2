4145c21a83dc65facef3378d8427c17b38815fc9
Sixten Paulsson <sixtenpaulsson@gmail.com>
2024-10-09 16:28:44 +0200

    improvements to funcs

--- matrix.py
+++ matrix.py
@@ -5,12 +5,18 @@ def powers(list=[], pow1=0, pow2=5):
         #Gör en ny rad som kommer bli inlagd i powerlist
         num_powers = []
         #Går igenom varje exponent
-        for i in range(pow2-pow1+1):
+        for i in range(pow1,pow2-pow1+1):
             #Lägger på varje exponent i num_powers
-            num_powers.append(num**i+pow1)
+            num_powers.append(num**i)
         #Lägger till listan i powerlist
         power_list.append(num_powers)
     return power_list
+
+
+#Transpose funktion
+
+#Hade kunnat vara bättre 
+#if(len(transposed)==y) raden är rätt ful
 
 def transpose(matrixes = []):
     transposed = []
@@ -30,10 +36,12 @@ def matmul(matrix1 = [], matrix2 = []):
     for m1_rows in range(len(matrix1)):
         new_matrix.append([])
         for m2_coloumns in range(len(matrix2)):
-            sum = 0
-            for m1_coloumns in range(len(matrix1[m1_rows])):
-                sum+=matrix1[m1_rows][m1_coloumns]*matrix2[m2_coloumns][m1_coloumns]
-            new_matrix[m1_rows].append(sum) 
+            #Gör en lambda funktion som gångrar 2 element
+            #Map går igenom varje par element och slänger in dom i lambda funktionen
+            #Summerar resultatet
+            summa = sum(map(lambda x,y: x*y , matrix1[m1_rows], matrix2[m2_coloumns]))
+            #Slänger in summan i raden
+            new_matrix[m1_rows].append(summa) 
     return new_matrix
 
 def invert(matrix = [[0,0],[0,0]]):
@@ -42,6 +50,8 @@ def invert(matrix = [[0,0],[0,0]]):
     return [
         [matrix[1][1]/det,-matrix[0][1]/det],
         [-matrix[1][0]/det,matrix[0][0]/det]]
+
+#Fullösning på loadtxt, funkar men för många rader
 def loadtxt(filename = ""):
     file = open(filename)
     lines = file.readlines()
@@ -51,3 +61,4 @@ def loadtxt(filename = ""):
         lines[i][1] = float(lines[i][1])
     file.close()
     return lines
+
