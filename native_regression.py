34cfe70496ec5fb51de2c0249e6465ccef7cf40e
Sixten Paulsson <sixtenpaulsson@gmail.com>
2024-10-09 16:05:09 +0200

    begginning of native regression file

---
+++ native_regression.py
@@ -0,0 +1,8 @@
+from matrix import *
+#import matplotlib.pyplot as plt
+
+#Xp  = powers(X,0,1)
+#Yp  = powers(Y,1,1)
+#Xpt = transpose(Xp)
+
+#[[b],[m]] = matmul(invert(matmul(Xpt,Xp)),matmul(Xpt,Yp))
