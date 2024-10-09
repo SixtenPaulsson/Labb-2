2cec069f39ef5d7c78bcb95e4ebfcc7ca59e78dc
Sixten Paulsson <sixtenpaulsson@gmail.com>
2024-10-09 13:57:04 +0200

    Working transpose function, needs improving though

--- matrix.py
+++ matrix.py
@@ -1,13 +1,21 @@
-def powers(list, num1, num2):
-    pass
-def transpose(matrix = []):
-    pass
-def matmul(matrix1 = [], matrix2 = []):
-    pass
-def invert(matrix = []):
-    pass
-def loadtxt(filename = ""):
-    file = open(filename)
-    print(file.read)
-    file.close()
-    pass
+#def powers(list, num1, num2):
+#    pass
+def transpose(matrixes = []):
+    transposed = []
+    for i in range(len(matrixes)):
+        for y in range(len(matrixes[0])):
+            #Lite av en fullösning här, ska fixa
+            if len(transposed)==y: transposed.append([])
+            transposed[y].append(matrixes[i][y])
+    print(transposed)
+    return transposed
+
+#def matmul(matrix1 = [], matrix2 = []):
+#    pass
+#def invert(matrix = []):
+#    pass
+#def loadtxt(filename = ""):
+#    file = open(filename)
+#    print(file.read)
+#    file.close()
+#    pass
