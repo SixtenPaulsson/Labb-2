1e1209e651df08d0246d822f975a963c8112387a
Sixten Paulsson <sixtenpaulsson@gmail.com>
2024-10-11 14:04:03 +0200

    nativa saker

--- native_regression.py
+++ native_regression.py
@@ -1,24 +1,33 @@
 from matrix import *
-#import matplotlib.pyplot as plt
-#import numpy
+import matplotlib.pyplot as plt
 import sys
 
 def main():
-    print(sys.argv)
+    #print(sys.argv)
 
    
     data = loadtxt(sys.argv[1])
     data = transpose(data)
-    print(data)
+    #print(data)
     X=data[0]
     Y=data[1]
     #print("X = ",X,"Y =",Y)
     Xp  = powers(X,0,1)
     Yp  = powers(Y,1,1)
     Xpt = transpose(Xp)
-    print(Xp,"asd",Yp,"Sak")
+    #print(Xp,"asd",Yp,"Sak")
     [[b],[m]]  = matmul(invert(matmul(Xpt,Xp)),matmul(Xpt,Yp))
-    print(b+m*float(sys.argv[2]))
+    #print(b+m*float(sys.argv[2]))
+    #Y2 = map(lambda x: b + m*x,X)
+    print(X,Y)
+    Y2= []
+    for i in X:
+        Y2.append(b+m * i)
+    plt.plot(X,Y,'ro')
+    plt.plot(X,Y2)
+    print(Y2)
+    plt.show()
+    print(Y2)
 
 
 if __name__ == "__main__":
